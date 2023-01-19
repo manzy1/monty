721d58350485ba796852ce75cde864b166535254
emmanuel <mziyaemmanuel@gmail.com>
2023-01-19 15:18:12 -0800

    pint

--- op_pint.c
+++ op_pint.c
@@ -13,5 +13,4 @@ void pint(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 		handle_error(ERR_PINT_USG, NULL, line_number, NULL);
 
-	printf("%d\n", (*stack)->n);
-}
+	printf("%d\n", (*stack)->n);}
